d73565e26a413e459101822a795bd5f071d7a362
shivrjpatil <shivrajp972@gmail.com>
2022-11-10 18:53:01 +0530

    [shiv]add:folder structure

---
+++ SnakeAndLadderProblem/GamePlay.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace SnakeAndLadderProblem
+{
+    public class GamePlay
+    {
+        public void Position()
+        {
+            int playerPosition = 0;
+            Console.WriteLine("Lets Play!!!" + "\t" + "You are at - " + playerPosition);
+        }
+    }
+}
---
+++ SnakeAndLadderProblem/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace SnakeAndLadderProblem
+
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Snake and Ladder Problem Statement");
+
+            GamePlay playerPosition = new GamePlay();
+            playerPosition.Position();
+
+        }
+    }
+}
