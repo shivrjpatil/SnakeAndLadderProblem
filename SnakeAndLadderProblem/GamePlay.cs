8cd20c1d43dcdd682203326b041190becb8cf1ae
shivrjpatil <shivrajp972@gmail.com>
2022-11-10 19:51:20 +0530

    [shiv]add:count of dice

--- SnakeAndLadderProblem/GamePlay.cs
+++ SnakeAndLadderProblem/GamePlay.cs
@@ -3,10 +3,50 @@ namespace SnakeAndLadderProblem
 {
     public class GamePlay
     {
+        Random random = new Random();
+        const int NO_PLAY = 0, LADDER = 1, SNAKE = 2, WINNING_POSITION = 100;
+        int playerPosition = 0, count = 0;
+
         public void Position()
         {
-            int playerPosition = 0;
             Console.WriteLine("Lets Play!!!" + "\t" + "You are at - " + playerPosition);
+        }
+        public int DiceRoll()
+        {
+            int dice = random.Next(1, 7);
+            Console.WriteLine("Output of Dice - " + dice);
+            _ = this.playerPosition;
+            count++;
+            return dice;
+        }
+        public void Check()
+        {
+            while (playerPosition < WINNING_POSITION)
+            {
+                int ROLL = random.Next(0, 3);
+                Console.WriteLine(ROLL);
+                switch (ROLL)
+                {
+                    case NO_PLAY:
+                        this.playerPosition += 0;
+                        break;
+                    case LADDER:
+                        int die = DiceRoll();
+                        if (this.playerPosition + die <= WINNING_POSITION)
+                            this.playerPosition += die;
+                        break;
+                    case SNAKE:
+                        this.playerPosition -= DiceRoll();
+                        if (this.playerPosition < 0)
+                        {
+                            this.playerPosition = 0;
+                        }
+                        break;
+                }
+                Console.WriteLine("You are at " + this.playerPosition);
+            }
+            Console.WriteLine("Total dice count will be - " + count);
+            Console.WriteLine("Yuppieee !! You won the game");
         }
     }
 }
